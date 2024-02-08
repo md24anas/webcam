233019867dddd871dee6b38c1e21bcdad8e90a0d
md24anas <md24anas@gmail.com>
2024-02-07 19:31:24 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import Camvideo from "./Camvideo";
 import './App.css';
-
 function App() {
 
   return (
-    <div className="App">
+    <div className="App"  >
       <Camvideo />
     </div>
   );
